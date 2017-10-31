492701b1dda72c09b1d4b775919f5993e7de1dda
Administrator <2514211828@qq.com>
2017-10-31 12:38:53 +0800

    1

--- sheets.py
+++ sheets.py
@@ -1,9 +1,41 @@
 # coding: utf-8
 from openpyxl import load_workbook
 from datetime import date, timedelta
+from openpyxl.styles import Border, Side
 
 wb = load_workbook('greens.xlsx')
 ws1 = wb.active
+
+
+def set_border(ws, cell_range):
+    rows = ws[cell_range]
+    side = Side(border_style='thin', color="FF000000")
+
+    rows = list(rows)  # we convert iterator to list for simplicity, but it's not memory efficient solution
+    max_y = len(rows) - 1  # index of the last row
+    for pos_y, cells in enumerate(rows):
+        max_x = len(cells) - 1  # index of the last cell
+        for pos_x, cell in enumerate(cells):
+            border = Border(
+                left=cell.border.left,
+                right=cell.border.right,
+                top=cell.border.top,
+                bottom=cell.border.bottom,
+            )
+            if pos_x == 0:
+                border.left = side
+            if pos_x == max_x:
+                border.right = side
+            if pos_y == 0:
+                border.top = side
+            if pos_y == max_y:
+                border.bottom = side
+            border.vertical = side
+            border.horizontal = side
+
+            # set new border only if it's one of the edge cells
+            if pos_x == 0 or pos_x == max_x or pos_y == 0 or pos_y == max_y:
+                cell.border = border
 
 ws2 = wb.copy_worksheet(ws1)
 ws3 = wb.copy_worksheet(ws1)
@@ -27,6 +59,12 @@ ws4['A31'] = '检测日期：%s' % str(date.today()+timedelta(3)) + '   ' + '星
              '   ' + '检测员：丁杰俊 廖娟'
 ws5['A31'] = '检测日期：%s' % str(date.today()+timedelta(4)) + '   ' + '星期%s' % dicts[str((date.today()+timedelta(4)).isoweekday())] + \
              '   ' + '检测员：丁杰俊 廖娟'
+
+set_border(ws1, 'A1:G32')
+set_border(ws2, 'A1:G32')
+set_border(ws3, 'A1:G32')
+set_border(ws4, 'A1:G32')
+set_border(ws5, 'A1:G32')
 
 wb.save('%s' % date.today() + '-' '%s' % (date.today()+timedelta(4)) + u'蔬菜及水果类.xlsx')
 
